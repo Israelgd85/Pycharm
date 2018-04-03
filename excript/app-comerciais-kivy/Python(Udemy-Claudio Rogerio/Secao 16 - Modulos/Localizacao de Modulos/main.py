06e5fc94396500286e7847c4455b7dc9cc7f8e1e
Israel <gomesdeconto@gmail.com>
2018-04-03 11:30:23 -0300

    update

--- excript/app-comerciais-kivy/Python(Udemy-Claudio Rogerio/Secao 16 - Modulos/Localizacao de Modulos/main.py
+++ excript/app-comerciais-kivy/Python(Udemy-Claudio Rogerio/Secao 16 - Modulos/Localizacao de Modulos/main.py
@@ -1,9 +1,24 @@
-import math
+#cofing: utf-8
 
-e = math.e
 
-pi = math.pi
 
-print(e)
+#from sys import  path as lpath
+from pprint import pprint
+import sys
+import ferramentas
+pprint(sys.path)
 
-print(pi)
+
+
+# ['/home/israel/dev/excript/app-comerciais-kivy/Secao 16 - Modulos',
+#  '/home/israel/dev/excript/app-comerciais-kivy',
+#  '/home/israel/dev/excript/app-comerciais-kivy/Secao 16 - Modulos/Criacao  de 'Modulos',
+#  '/home/israel/dev/excript/app-comerciais-kivy/Secao 16 - Modulos/Localizacao de Modulos',
+
+#  '/home/israel/.virtualenvs/k35/lib/python35.zip',
+#  '/home/israel/.virtualenvs/k35/lib/python3.5',
+#  '/home/israel/.virtualenvs/k35/lib/python3.5/plat-x86_64-linux-gnu',
+#  '/home/israel/.virtualenvs/k35/lib/python3.5/lib-dynload',
+#  '/usr/lib/python3.5',
+#  '/usr/lib/python3.5/plat-x86_64-linux-gnu',
+#  '/home/israel/
