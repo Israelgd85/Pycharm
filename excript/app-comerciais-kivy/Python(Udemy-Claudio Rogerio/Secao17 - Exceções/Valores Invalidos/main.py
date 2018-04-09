fcf60df2b20b9728764c0fecde8a5cc56ff702bf
Israel <gomesdeconto@gmail.com>
2018-04-09 16:16:20 -0300

    Starting cod. Add Function input_float

--- excript/app-comerciais-kivy/Python(Udemy-Claudio Rogerio/Secao17 - Exceções/Valores Invalidos/main.py
+++ excript/app-comerciais-kivy/Python(Udemy-Claudio Rogerio/Secao17 - Exceções/Valores Invalidos/main.py
@@ -1,3 +1,10 @@
+def input_float(msg):
+    while True:
+        try:
+            return float(input(msg))
+        except ValueError:
+            print("Número Inválido")
+
 num1 = float(input("Digite o primeiro numero: "))
 num2 = float(input("Digite o segundo numero: "))
 
