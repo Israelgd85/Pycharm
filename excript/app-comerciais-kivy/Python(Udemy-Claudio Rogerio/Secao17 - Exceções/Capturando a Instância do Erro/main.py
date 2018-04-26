35683897c16e9752b283b012fb0fb058301205ff
Israel <gomesdeconto@gmail.com>
2018-04-26 15:03:08 -0300

    Add comments

--- excript/app-comerciais-kivy/Python(Udemy-Claudio Rogerio/Secao17 - Exceções/Capturando a Instância do Erro/main.py
+++ excript/app-comerciais-kivy/Python(Udemy-Claudio Rogerio/Secao17 - Exceções/Capturando a Instância do Erro/main.py
@@ -3,12 +3,16 @@ def erro(x):
         eval(x)
     except ValueError as e:
         print("ValueError")
-        print(type(e))
+        print(type(e)) #instância da exceção
+        print(e.args)  #argumentos as mensagens
+        print(e)       #__str__mensagem
     except ZeroDivisionError:
         print("ZeroDivisionError")
-    except (TypeError, NameError):
+    except (TypeError, NameError)as e:
         print("NameErros ocorreu ou então, NameError")
-
+        print(type(e))  # instância da exceção
+        print(e.args)   # argumentos as mensagens
+        print(e)        # __str__mensagem
 
 
 
@@ -16,13 +20,13 @@ def erro(x):
 erro("int+int")
 
 #NameError
-erro("a")
+#erro("a")
 
 #ValueError
 erro("int('a')")
 
 #ZeroDivisionError
-erro("5/0")
-erro("10+10")
+#erro("5/0")
+#erro("10+10")
 
 print("A aplicação foi finalizada")
