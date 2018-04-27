d5a4753058a1e741fdb0e8e118621529ef6f6800
Israel <gomesdeconto@gmail.com>
2018-04-27 10:23:14 -0300

    Start Classe Retangulo

---
+++ excript/app-comerciais-kivy/Python(Udemy-Claudio Rogerio/Secao18 - Orientacao a Objetos I/Classe Retângulo/main.py
@@ -0,0 +1,16 @@
+class Retagulo:
+
+    def __init__(self):
+        self.a = 0
+        self.l = 0
+
+    def area(self):
+        return self.a * self.l
+
+r1 = Retagulo()
+r1.l = 10
+r1.a = 5
+
+print(r1.area())
+
+
