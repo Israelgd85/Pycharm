0fa2a934f07efcff890a176550c8d8ff0a09eca0
Israel <gomesdeconto@gmail.com>
2018-04-27 11:12:03 -0300

    Start Encapsulamento

---
+++ excript/app-comerciais-kivy/Python(Udemy-Claudio Rogerio/Secao19 - Orientacao  a Objetos II/Encapsulamento/main.py
@@ -0,0 +1,16 @@
+#coding: utf-8
+
+__author__= "Israel Gomes Deconto"
+
+class Retangulo:
+
+    def __init__(self, largura, altura):
+        self.l = largura
+        self.a = altura
+
+    def area(self):
+        return self.l * self.a
+
+r = Retangulo(10, 5)
+r.l = "teste"
+print(r.area())
