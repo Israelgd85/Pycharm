f75c37d243103721a90471725ae723d16599c06d
Israel <gomesdeconto@gmail.com>
2018-07-23 16:13:02 -0300

    hello world (kivlang)

---
+++ excript/app-comerciais-kivy/Python(Udemy-Claudio Rogerio/Secao22 - Introducao a kvlang/Hello world com Kvlang/main.py
@@ -0,0 +1,13 @@
+#conding: utf-8
+
+from kivy.app import App
+#from kivy.uix.label import Label
+
+
+class HelloApp(App):
+    pass
+    # def build(self):
+    #     pass
+        # return Label(text="Hello World")
+
+HelloApp().run()
